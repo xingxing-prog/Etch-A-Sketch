edf07b421266a83123943a70c37a40c5ca439018
MX <dinosaurbao@pm.me>
2022-07-09 11:52:36 -0400

    it has achieved basic function for sketch. but main functions are blended and twisted in getGridSize() this function. which is not good for code managment and understanding or code reusable.

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 var container = document.getElementById("container");
 var myRange = document.getElementById("myRange");
 var slider = document.getElementById("slider");
-var cells = document.getElementsByClassName("cell"); 
+var clear = document.getElementById("clear");
+var eraser = document.getElementById("eraser");
+var currentColor = document.getElementById("currentColor");
 
 
 function makeColumns(rows, cols){
@@ -10,36 +12,69 @@ function makeColumns(rows, cols){
     
     for (let i=0; i<(rows*cols);i++){
         let cell=document.createElement('div'); 
-        container.appendChild(cell).className="cell";
-        
+        container.appendChild(cell).className="cell"; 
     }
-    
+
 
 }
 
 
 
-
-function getGridSize(e){
-
-    e.preventDefault();
+function getGridSize(){
 
     var col = myRange.value;
-    var rangeValue =col;
-    makeColumns(rangeValue, rangeValue);
-    var newItem = col +" x "+ col;
+  
+    makeColumns(myRange.value, myRange.value);
+    var newItem = myRange.value +" x "+ myRange.value;
 
     slider.textContent =newItem;
-    console.log(col);
-    
-    
-}
 
-myRange.addEventListener('change', getGridSize);
 
-cells.forEach((cell)=> {
-    cell.addEventListener('click', ()=>{
-        cell.style.backgroundColor="green"});
+    var cells =document.querySelectorAll(".cell");
+
+    cells.forEach((cell)=>{
+        cell.style.backgroundColor="white";
+    });
+
+
+
+    cells.forEach((cell)=>{
+        
+        
+        currentColor.addEventListener("input", ()=>{
+            eraser.addEventListener('click', ()=>{
+                cell.addEventListener('mousedown', ()=>{
+                    if(cell.style.backgroundColor != "white"){
+                        cell.style.backgroundColor = "white";
+                    }
+                    else{
+                        cell.style.backgroundColor ="white";
+                    }
+                })
+            });
+            cell.addEventListener('mousedown', ()=>{
+                
+                cell.style.backgroundColor =currentColor.value;
+            });
+            
+            
+            clear.addEventListener('click', ()=>{
+                cell.style.backgroundColor="white";
+
+              
+            });
+
+        })
+
+       
     })
+
+
+
+}
+getGridSize();
+
+myRange.addEventListener('click', getGridSize);
+
 
 
